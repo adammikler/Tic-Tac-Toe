634d211b796e5512b6d707430b50eb88465c19df
adam mikler <adammikler10@gmail.com>
2022-10-09 11:18:37 -0700

    unpolished v1
    finished rough copy. all major functions worrking, styling uncomplete.

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let board = Array.apply(null, Array(9));
+let board = Array.apply(null, Array(9)); /*Creates the Array to store moves*/
 
+/*Creates the player objects with there abilites*/
 const Player = (piece, turn) => {
+    /*updates the current player and highlights whos move it is*/
     const updateTurn = (piece) => {
         if (piece.turn == 1) {
+
            piece.turn = 0;
+
+           const active = document.querySelector('.active');
+           const notActive = document.querySelector('.not-active');
+
+           active.classList.replace('active', 'not-active');
+           notActive.classList.replace('not-active', 'active');
         } else {
             piece.turn = 1;
         };
     };
+    /*Takes in themove and updates it with a 1 for X or -1 for O*/
     const makeMove = (move) => {
         if (piece == 'x') {
             board[move] = 1;
@@ -15,7 +25,7 @@ const Player = (piece, turn) => {
             board[move] = -1;
         };
     };
-    return {piece, turn, updateTurn, makeMove};
+    return {piece, turn, updateTurn, makeMove};/*Object created*/
 };
 
 function playMove(x, o, i) {
@@ -38,45 +48,62 @@ function playMove(x, o, i) {
 };
 
 function checkWin() {
-    if (board[0] == 1, board[1] == 1, board[2] == 1) {
-        return(console.log('x wins'));
-    } else if (board[0] == -1 && board[1] == -1 && board[2] == -1) {
-        return(console.log('o wins'));
-    } else if (board[3] == 1 && board[4] == 1 && board[5] == 1) {
-        return(console.log('x wins'));
-    } else if (board[3] == -1 && board[4] == -1 && board[5] == -1) {
-        return(console.log('o wins'));
-    } else if (board[6] == 1 && board[7] == 1 && board[8] == 1) {
-        return(console.log('x wins'));
-    } else if (board[6] == -1 && board[7] == -1 && board[8] == -1) {
-        return(console.log('x wins'));
-    } else if (board[0] == 1 && board[3] == 1 && board[6] == 1) {
-        return(console.log('x wins'));
-    } else if (board[0] == -1 && board[3] == -1 && board[6] == -1) {
-        return(console.log('o wins'));
-    } else if (board[1] == 1 && board[4] == 1 && board[7] == 1) {
-        return(console.log('x wins'));
-    } else if (board[1] == -1 && board[4] == -1 && board[7] == -1) {
-        return(console.log('o wins'));
-    } else if (board[2] == 1 && board[5] == 1 && board[8] == 1) {
-        return(console.log('x wins'));
-    } else if (board[2] == -1 && board[5] == -1 && board[8] == -1) {
-        return(console.log('o wins'));
-    } else if (board[0] == 1 && board[4] == 1 && board[8] == 1) {
-        return(console.log('x wins'));
-    } else if (board[0] == -1 && board[4] == -1 && board[8] == -1) {
-        return(console.log('o wins'));
-    } else if (board[2] == 1 && board[4] == 1 && board[6] == 1) {
-        return(console.log('x wins'));
-    } else if (board[2] == -1 && board[4] == -1 && board[6] == -1) {
-        return(console.log('0 wins'));
-    } else {
-        return true;
+    /*used to measure if a row or collumn combined makes a win*/
+    xwin = 3;
+    owin = -3;
+    /*checks for row wins*/
+    for (let i = 0; i < 9; i+=3) {
+        console.log(i);
+        if(board[0 + i] + board [1 + i] + board[2 + i] == xwin) {
+            displayWinner('X wins');
+            clearBoard();
+        } else if (board[0 + i] + board [1 + i] + board[2 + i] == owin) {
+            displayWinner('O wins');
+            clearBoard();
+        };
+    };
+    /*checks for collumn wins*/
+    for (let i = 0; i < 3; i++) {
+        if(board[0 + i] + board [3 + i] + board[6 + i] == xwin) {
+            displayWinner('X wins');
+            clearBoard();
+        } else if (board[0 + i] + board [3 + i] + board[6 + i] == owin) {
+            displayWinner('O wins');
+            clearBoard();
+        };
+    };
+    /*checks for diagonal wins*/
+    if (board[0] + board[4 ] + board[8] == xwin) {
+        displayWinner('X wins');
+        clearBoard();
+    } else if (board[0] + board[4 ] + board[8] == owin) {
+        displayWinner('O wins');
+        clearBoard();
+    } else if (board[2] + board[4 ] + board[6] == xwin) {
+        displayWinner('X wins');
+        clearBoard();
+    } else if (board[2] + board[4 ] + board[6] == owin) {
+        displayWinner('O wins');
+        clearBoard();
     };
 };
-    
+/*displays who the winner is*/
+function displayWinner(winner) {
+    document.getElementById('winner').innerText = winner;
+}
+/*Clears the board and array*/
+function clearBoard() {
+    let button = document.getElementById(0);
+    for (let i = 0; i < 9; i++) {
+        button = document.getElementById(i);
+        button.innerHTML = '';
+    };
+    board.length = 0;
+    board = Array.apply(null, Array(9));
+}
+
 function Game() {
-    const x = Player('x', 1);
+    const x = Player('x', 1);/*Creates both players */
     const o = Player('o', 0);
 
     let button = document.getElementById(0);
